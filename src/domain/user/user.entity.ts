bdeff43f40f3005bcd6977e568325276442a653b
keshavsaini-22 <keshav.1147@zenmonk.tech>
2024-11-14 17:51:08 +0530

    Entity bnayo

---
+++ src/domain/user/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column("varchar", { length: 100 })
+    name: string
+
+    @Column("varchar", { length: 100 })
+    email: string
+}
